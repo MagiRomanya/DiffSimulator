Fixex initial velocities jacobian bug

differentiable/save_simulation_velocities.py
@@ -27,10 +27,11 @@ if __name__ == "__main__":
     sim = Simulation()
 
     n_tension, n_bending = sim.getSpringNumbers()
-    param = np.genfromtxt("paramters_gioconda.csv", delimiter=",")
-    k = param[0]
-    k_bend = param[1]
-    vel = param[2:]
+    param = np.genfromtxt("paramters_gioconda_xd.csv", delimiter=",")
+    print(len(param))
+    k = 0
+    k_bend = 0
+    vel = param
 
     sim = Simulation(0,0, True)
     sim.reset_simulation(k, k_bend, vel)

differentiable/test.py
@@ -2,10 +2,117 @@
 
 from symulathon import Simulation
 import numpy as np
+from tqdm.notebook import tqdm
+from scipy.optimize import minimize, Bounds
+from simulation_functions import newton_iteration
+from recorder import SimulationReader
+from backpropagation import Backpropagation
+k = 100
+k_bend = 0.1
+graphics=True
+sim = Simulation(k, k_bend, 0, graphics)
+nDoF = sim.getDoF()
+DIFF_FRAMES = 100
 
-sim = Simulation()
-ndof = sim.getDoF()
+def simulate(initial_velocities: np.array, diff_frames: int):
+    nDoF = sim.getDoF()
+    mass = sim.getMassMatrix()
+    h = sim.getTimeStep()
+    reader = SimulationReader(nDoF,"record_shape.csv")
+    sim.reset_simulation(initial_velocities)
+    dx0dp = sim.getInitialPositionJacobian()
+    dv0dp = sim.getInitialVelocityJacobian()
+    backpropagation = Backpropagation(mass, dx0dp, dv0dp, h)
+    sim.fill_containers()
+    for i in range(DIFF_FRAMES+1):
+        ##################################
+        # Record step for backpropagation
+        ##################################
+        x = sim.getPosition()
+        v = sim.getVelocity()
+        x_t, v_t = reader.get_next_state()
+        A = sim.getEquationMatrix()
+        dfdp = sim.getParameterJacobian()
+        dfdx = sim.getForcePositionJacobian()
+        backpropagation.step(x, v, x_t, v_t, A, dfdp, dfdx)
 
-initial_velocities = np.zeros(ndof)
+        ##################################
+        # Newton Iterations
+        ##################################
+        iterations = 3
+        xi = x
+        vi = v
+        for it in range(iterations):
+            xi, vi = newton_iteration(sim, x, v, xi, vi)
+            sim.set_state(xi, vi)
+            sim.fill_containers()
 
-sim.reset_simulation(initial_velocities)
+        sim.render_state()
+    return backpropagation
+
+def simulate2(k: float, k_bend: float, initial_velocities: np.array, diff_frames: int):
+    nDoF = sim.getDoF()
+    mass = sim.getMassMatrix()
+    h = sim.getTimeStep()
+    reader = SimulationReader(nDoF,"record_shape.csv")
+    sim.reset_simulation(k, k_bend, initial_velocities)
+    dx0dp = sim.getInitialPositionJacobian()
+    dv0dp = sim.getInitialVelocityJacobian()
+    backpropagation = Backpropagation(mass, dx0dp, dv0dp, h)
+    sim.fill_containers()
+    for i in range(DIFF_FRAMES+1):
+        ##################################
+        # Record step for backpropagation
+        ##################################
+        x = sim.getPosition()
+        v = sim.getVelocity()
+        x_t, v_t = reader.get_next_state()
+        A = sim.getEquationMatrix()
+        dfdp = sim.getParameterJacobian()
+        dfdx = sim.getForcePositionJacobian()
+        backpropagation.step(x, v, x_t, v_t, A, dfdp, dfdx)
+
+        ##################################
+        # Newton Iterations
+        ##################################
+        iterations = 3
+        xi = x
+        vi = v
+        for it in range(iterations):
+            xi, vi = newton_iteration(sim, x, v, xi, vi)
+            sim.set_state(xi, vi)
+            sim.fill_containers()
+
+        sim.render_state()
+    return backpropagation
+
+initial_velocities = np.zeros(nDoF)
+initial_tension = np.array([100])
+initial_bending = np.array([0.1])
+parameters = np.concatenate((initial_tension, initial_bending, initial_velocities))
+print(parameters)
+
+def simulation_wrapper(initial_velocities):
+    bp = simulate2(0, 0, initial_velocities, DIFF_FRAMES)
+    g = bp.get_g()
+    dgdp = bp.get_dgdp()[3:]
+    return g, dgdp
+
+def simulation_wrapper2(paramters):
+    k = paramters[0]
+    k_bend = paramters[1]
+    ivel = paramters[2:]
+    bp = simulate2(k, k_bend, ivel, DIFF_FRAMES)
+    g = bp.get_g()
+    dgdp = bp.get_dgdp()
+    dgdp = np.delete(dgdp, 2) # remove garbage paramter
+    return g, dgdp
+
+print(len(parameters))
+res = minimize(simulation_wrapper, initial_velocities, jac=True,
+               bounds=Bounds(-100, 100),
+               options={"disp": True,
+                        "maxiter": 500,
+                       })
+print(res)
+print(res.x)

differentiable/visualize_shapes.py
@@ -17,20 +17,21 @@ side_length = 5
 n_nodes = 20
 sphere_radius = side_length / (2.*np.pi)
 
-dangle = 2.*np.pi / 20
-angle = np.pi / 2. + dangle / 2.
 
 def sphere_point(angle):
     return sphere_radius * np.cos(angle), start_y + sphere_radius * np.sin(angle)
 
 # We are working in the x-y plane, z axis is depth
-# for i in range(0, len(x), 3):
-#     print(f"x={x[i]}, y={x[i+1]}, z={x[i+2]}")
+def make_cilinder():
+    dangle = 2.*np.pi / 20
+    angle = np.pi / 2. + dangle / 2.
+    for i in range(0, len(x), 3):
+        print(f"x={x[i]}, y={x[i+1]}, z={x[i+2]}")
 
-#     xx, yy = sphere_point(angle)
-#     x[i] = xx
-#     x[i+1] = yy
-#     angle += dangle
+        xx, yy = sphere_point(angle)
+        x[i] = xx
+        x[i+1] = yy
+        angle += dangle
 
 def rotate_vertices(x: np.array, angle: float):
     y_offset = 10
@@ -58,7 +59,9 @@ def rotate_vertices(x: np.array, angle: float):
         x[i+1] = new_pos[1] +y_offset
         x[i+2] = new_pos[2]
 
-rotate_vertices(x, np.pi/2)
+
+make_cilinder()
+# rotate_vertices(x, np.pi/2)
 sim.set_state(x, np.zeros(dof))
 
 for i in range(100):